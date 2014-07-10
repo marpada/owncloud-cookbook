5cd593131483385449ff3b5c7869eadbc4538790
David Pando <david.pando@gmail.com>
2014-07-10 21:18:02 +1200

    PHP refactor
    * Use php-fpm cookbook instead of priestjim/chef-php
    * Support for php5enmod (PHP >= 5.4)

--- recipes/php.rb
+++ recipes/php.rb
@@ -1,29 +1,41 @@
 include_recipe 'php'
-include_recipe 'php::fpm'
+include_recipe 'php-fpm'
 
+php_fpm_pool "www" do
+    enable false
+end
 
-  php_fpm 'owncloud' do
-    action :add
-    socket true
-    user node['nginx']['user']
-    group node['nginx']['group']
-    socket_path "/var/run/owncloud.sock"
-    start_servers 1
-    max_children node['owncloud']['fpm']['max_children']
-    min_spare_servers 1
-    max_spare_servers 1
-  value_overrides({
-  :error_log => "#{node['php']['fpm_log_dir']}/owncloud.log"
-  })
-  end
+php_options = ''
 
-%w{curl gd mbstring mcrypt memcache mhash mysql xml apc}.each do |m|
-  include_recipe "php::module_#{m}"
+node['owncloud']['php']['options'].each do |k,v|
+  php_options << "php_admin_value[#{k}] => '#{v}', "
+end
+
+pool_php_options = {}
+
+node['owncloud']['php']['options'].each do |k,v|
+  pool_php_options["php_admin_value[#{k}]"] = v
 end
 
 
-%w{php5-imagick }.each do |p|
-  package p do
+php_fpm_pool "owncloud" do
+    process_manager node['owncloud']['php']['process_manager']
+    listen  node['owncloud']['php']['fpm_socket']
+    max_children  node['owncloud']['php']['max_children']
+    min_spare_servers  node['owncloud']['php']['min_spare_servers']
+    max_spare_servers  node['owncloud']['php']['max_spare_servers']
+    php_options pool_php_options
+end
+
+## Install and configure required php-modules
+%w{mcrypt gd mysql curl imap xcache imagick mhash }.each do |m|
+  package "php5-#{m}" do
     action :install
   end
+
+    execute "enable php5-#{m}" do
+      command "php5enmod #{m}"
+      only_if "which php5enmod"
+    end
+  #end
 end
